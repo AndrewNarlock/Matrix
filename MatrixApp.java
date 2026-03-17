77f97e1c9b1c08abc8e19912baac83a2b3b0a3cf
AndrewNarlock <95453161+AndrewNarlock@users.noreply.github.com>
2026-03-16 20:08:01 -0700

    Update MatrixApp.java
    Added some outputs for testing.

--- MatrixApp.java
+++ MatrixApp.java
@@ -7,6 +7,31 @@ public class MatrixApp {
         System.out.println(m.toString());
         Matrix n = new Matrix(5, 7, 1, 2, 3, 4, 5);
         System.out.println(n.toString());
+
+        Matrix l = new Matrix(5, 7, 1, 2, 3, 4, 5);
+        //m.add(n);
+        Matrix result = n.add(l);
+
+        System.out.println(result.toString());
+
+        Matrix sub = new Matrix(5, 7, 2, 4, 6, 8, 10);
+        Matrix result2 = result.subtract(sub);
+
+        System.out.println(result2.toString());
+
+        Matrix mult = sub.multiply(3);
+        System.out.println(mult.toString());
+
+        Matrix doubmult = sub.multiply(3.2);
+        System.out.println(doubmult.toString());
+
+        Matrix A = new Matrix(3, 2, 1, 2, 3, 4, 5, 6);
+        Matrix B = new Matrix(2, 3, 1, 2, 3, 4, 5, 6);
+
+        Matrix C = A.multiply(B);
+
+        System.out.println(C.toString());
+
     }
 
 
